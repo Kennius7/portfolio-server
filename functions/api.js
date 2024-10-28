31ac29df409b715bdcc3e82216ed220c66965e5f
Kennius7 <ogbogukenny@yahoo.com>
2024-10-28 16:01:24 +0100

    server edits - 03

--- functions/api.js
+++ functions/api.js
@@ -8,6 +8,12 @@ const app = express();
 const router =  express.Router()
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    next();
+})
 
 
 router.get('/', (req, res) => {
