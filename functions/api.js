44d787de1995e8784173b0ad58635760d18f3304
Kennius7 <ogbogukenny@yahoo.com>
2024-10-28 15:41:58 +0100

    server edits - 02

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
+const nodemailer = require('nodemailer');
 
 
 const app = express();
@@ -9,26 +10,68 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 router.get('/', (req, res) => {
     res.status(200).json({ success: true, message: 'Hello, World! My name is Kenny' });
 });
 
+// Configure the email transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // e.g., 'gmail'
+    auth: {
+        user: 'shosanacodemia@gmail.com',
+        pass: 'mgkr dhey vfry zdrc',
+    },
+    tls: {
+        rejectUnauthorized: false, // Bypass SSL certificate validation
+    },
+});
+
+// Handle POST request to send emails 
+router.post('/send-email', (req, res) => {
+    const formData = req.body;
+    console.log(formData);
+    const subject = "Explore Stephen Feedback Emails";
+    const htmlEmail = `
+        <html>
+            <body>
+            <p>${formData.firstName} ${formData.lastName} just sent you a message with the following data:</p>
+            <p>Subject: ${formData.subject}</p>
+            <p>Message Body: ${formData.message}</p>
+            <p>His/Her Phone/WhatSapp number is: ${formData.phone}</p>
+            <p>His/Her Email address is: ${formData.email}</p>
+            </body>
+        </html>
+    `;
+
+    const mailOptions = {
+        from: formData.email,
+        to: ['shosanacodemia@gmail.com', 'stephenojeogwu01@gmail.com'],
+        subject: subject,
+        html: htmlEmail,
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ 
+                success: false, 
+                timeoutMessage: "Error: Request Timed Out. Check your network and try again.",
+                errorMessage: "Error: Registration Failed.",
+            });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json({ 
+            success: true, 
+            emailMessage: `Email sent successfully: ${info.response}`, 
+            formMessage: 'Registration successful.'
+            });
+        }
+    });
+});
 
 
 app.use("/.netlify/functions/api", router);
 // eslint-disable-next-line no-undef
 module.exports.handler = serverless(app);
-
-
-
-
-
-
-
-
-
-
 
 
