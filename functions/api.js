53e5a4577bd13f663937d834b34e9efe8df13c5c
Kennius7 <ogbogukenny@yahoo.com>
2024-10-28 16:22:10 +0100

    server edits - 05

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 
 const app = express();
